f0cbb60f34d3b4ed1321b1978b14b267cbf38124
Lucas Módolo <lucasmg2001@gmail.com>
2021-10-03 22:12:31 -0300

    event handlerException

--- src/event/event.ts
+++ src/event/event.ts
@@ -1,4 +1,4 @@
-import { Client, Interaction, Message } from "discord.js";
+import {Client, CommandInteraction, Interaction, Message} from "discord.js";
 import { CommandFactory } from "../commands/commandFactory";
 import { environment } from "../environments/environment";
 import { InvalidCommand } from "../errors/invalidCommand";
@@ -25,8 +25,7 @@ export class Event {
 
             try{ this.commandFactory.factory(command).execute(message, args) }
             catch (e: unknown) {
-                if (e instanceof InvalidCommand) message.reply(e.message)
-                else message.reply("Something went wrong")
+                this.handlerException(message, e)
             }
         }
     }
@@ -35,10 +34,14 @@ export class Event {
         if (!interaction.isCommand()) return
 
         try { this.commandFactory.factory(interaction.commandName).execute(interaction, null) }
-        catch (e) {
-            if (e instanceof InvalidCommand) interaction.reply(e.message)
-            else interaction.reply("Something went wrong")
+        catch (e: unknown) {
+            this.handlerException(interaction, e)
         }
+    }
+
+    private handlerException(message: CommandInteraction | Message, exception: unknown): void {
+        if (exception instanceof InvalidCommand) message.reply(exception.message)
+        else message.reply("Something went wrong")
     }
 
 
