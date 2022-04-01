3812945124c197c60c9b5f7443b91bcd61f5d529
Lucas Módolo <lucasmg2001@gmail.com>
2022-04-01 19:26:56 -0300

    log commands

--- src/enumerations/logType.enum.ts
+++ src/enumerations/logType.enum.ts
@@ -2,5 +2,6 @@ export enum LogTypeEnum {
     JOIN_NEW_GUILD = 'joinNewGuild',
     REMOVE_GUILD = 'removeGuild',
     PLAY_MUSIC = 'playMusic',
-    ERROR = 'error'
+    ERROR = 'error',
+    COMMAND = "command",
 }
